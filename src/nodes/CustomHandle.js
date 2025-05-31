62de04572695fd2d28f7ec67353f008d73186bd0
kevalmiistry <kevalpm@gmail.com>
2025-05-31 20:15:25 +0530

    feat: adding support for one liner handle labels

--- src/nodes/CustomHandle.js
+++ src/nodes/CustomHandle.js
@@ -10,14 +10,14 @@ export const CustomHandle = ({ type, position, id, style = {}, label = "" }) => 
             position={position}
             id={id}
             className={cn(
-                "!w-3 !h-3 hover:scale-125 transition-all !bg-white !border !border-slate-600 !rounded-full ",
+                "!w-3 !h-3 hover:scale-125 transition-all !bg-white !border !border-slate-600 !rounded-full",
                 {
                     "!-translate-x-1/2 !left-0": type === "target",
                     "!translate-x-1/2 !right-0": type === "source",
-                    "after:content-[var(--handle-label)] after:absolute after:top-1/2 after:-translate-y-1/2":
+                    "after:content-[var(--handle-label)] after:absolute after:top-1/2 after:-translate-y-1/2 after:whitespace-nowrap":
                         !!label,
-                    "after:!-left-full after:-translate-x-3/4": label && type === "target",
-                    "after:!-right-full after:translate-x-3/4": label && type === "source",
+                    "after:!-left-full after:-translate-x-[85%]": label && type === "target",
+                    "after:!-right-full after:translate-x-[85%]": label && type === "source",
                 },
             )}
             style={{
